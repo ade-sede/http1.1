1438d6c9dcba0a7a85254497759ff2db4fa57ba0
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-06-28 14:47:10 +0200

    feat: basic OK response

--- src/main.zig
+++ src/main.zig
@@ -3,17 +3,13 @@ const std = @import("std");
 // const net = std.net;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!\n", .{});
-
     const address = try std.net.Address.resolveIp("127.0.0.1", 4221);
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
 
-    _ = try listener.accept();
-    try stdout.print("client connected!", .{});
+    var conn = try listener.accept();
+
+    _ = try conn.stream.write("HTTP/1.1 200 OK\r\n\r\n");
 }
