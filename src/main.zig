9240997a714cd3d15e38cd7fabb1d5aa156716c5
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-07-01 18:15:32 +0200

    feat: 1 connection = 1 thread

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
-const allocator = std.heap.page_allocator;
 
 const max_read_length = 1024;
 
 const Headers = struct {
+    allocator: std.mem.Allocator,
     raw: std.ArrayList([]const u8),
 
     pub fn deinit(self: *Headers) void {
         for (self.raw.items) |h| {
-            allocator.free(h);
+            self.allocator.free(h);
         }
 
         self.raw.deinit();
@@ -16,6 +16,7 @@ const Headers = struct {
 };
 
 const Request = struct {
+    allocator: std.mem.Allocator,
     method: []const u8,
     target: []const u8,
     // `segments` borrow from target
@@ -24,14 +25,16 @@ const Request = struct {
 
     headers: Headers,
 
-    pub fn init() !*Request {
+    pub fn init(allocator: std.mem.Allocator) !*Request {
         const request = try allocator.create(Request);
         request.* = Request{
+            .allocator = allocator,
             .method = undefined,
             .target = undefined,
             .http_version = undefined,
             .segments = std.ArrayList([]const u8).init(allocator),
             .headers = Headers{
+                .allocator = allocator,
                 .raw = std.ArrayList([]const u8).init(allocator),
             },
         };
@@ -43,28 +46,31 @@ const Request = struct {
         self.headers.deinit();
         self.segments.deinit();
 
-        allocator.free(self.method);
-        allocator.free(self.target);
-        allocator.free(self.http_version);
+        self.allocator.free(self.method);
+        self.allocator.free(self.target);
+        self.allocator.free(self.http_version);
 
-        allocator.destroy(self);
+        self.allocator.destroy(self);
     }
 };
 
 const Response = struct {
+    allocator: std.mem.Allocator,
     code: u64,
     http_version: []const u8,
     reason: []const u8,
     headers: Headers,
     body: ?[]const u8,
 
-    pub fn init() !*Response {
+    pub fn init(allocator: std.mem.Allocator) !*Response {
         const response = try allocator.create(Response);
         response.* = Response{
+            .allocator = allocator,
             .http_version = undefined,
             .code = undefined,
             .reason = undefined,
             .headers = Headers{
+                .allocator = allocator,
                 .raw = std.ArrayList([]const u8).init(allocator),
             },
             .body = undefined,
@@ -73,8 +79,8 @@ const Response = struct {
         return response;
     }
 
-    pub fn @"404"() !*Response {
-        var response = try Response.init();
+    pub fn @"404"(allocator: std.mem.Allocator) !*Response {
+        var response = try Response.init(allocator);
 
         response.code = 404;
         response.http_version = try allocator.dupe(u8, "HTTP/1.1");
@@ -82,8 +88,8 @@ const Response = struct {
         response.body = null;
     }
 
-    pub fn @"422"() !*Response {
-        var response = try Response.init();
+    pub fn @"422"(allocator: std.mem.Allocator) !*Response {
+        var response = try Response.init(allocator);
 
         response.code = 422;
         response.http_version = try allocator.dupe(u8, "HTTP/1.1");
@@ -91,8 +97,8 @@ const Response = struct {
         response.body = null;
     }
 
-    pub fn text(txt: []const u8) !*Response {
-        var response = try Response.init();
+    pub fn text(allocator: std.mem.Allocator, txt: []const u8) !*Response {
+        var response = try Response.init(allocator);
 
         response.code = 200;
         response.http_version = try allocator.dupe(u8, "HTTP/1.1");
@@ -105,7 +111,7 @@ const Response = struct {
         return response;
     }
 
-    fn packHeaders(headers: []const []const u8) ![]const u8 {
+    fn packHeaders(allocator: std.mem.Allocator, headers: []const []const u8) ![]const u8 {
         // A header section is of the form
         //
         // header_line\r\n
@@ -123,32 +129,32 @@ const Response = struct {
     }
 
     pub fn pack(self: *Response) ![]const u8 {
-        const status_line = try std.fmt.allocPrint(allocator, "{s} {d} {s}\r\n", .{ self.http_version, self.code, self.reason });
-        defer allocator.free(status_line);
+        const status_line = try std.fmt.allocPrint(self.allocator, "{s} {d} {s}\r\n", .{ self.http_version, self.code, self.reason });
+        defer self.allocator.free(status_line);
 
-        const header_section = try Response.packHeaders(self.headers.raw.items);
-        defer allocator.free(header_section);
+        const header_section = try Response.packHeaders(self.allocator, self.headers.raw.items);
+        defer self.allocator.free(header_section);
 
         if (self.body) |body| {
-            return std.mem.concat(allocator, u8, &[_][]const u8{ status_line, header_section, body });
+            return std.mem.concat(self.allocator, u8, &[_][]const u8{ status_line, header_section, body });
         }
 
-        return std.mem.concat(allocator, u8, &[_][]const u8{ status_line, header_section });
+        return std.mem.concat(self.allocator, u8, &[_][]const u8{ status_line, header_section });
     }
 
     pub fn deinit(self: *Response) void {
-        allocator.free(self.http_version);
-        allocator.free(self.reason);
+        self.allocator.free(self.http_version);
+        self.allocator.free(self.reason);
 
         if (self.body) |body| {
-            allocator.free(body);
+            self.allocator.free(body);
         }
 
         self.headers.deinit();
     }
 };
 
-fn readRequestLine(reader: *std.net.Stream.Reader) ![3][]u8 {
+fn readRequestLine(allocator: std.mem.Allocator, reader: *std.net.Stream.Reader) ![3][]u8 {
     const method = try reader.readUntilDelimiterOrEofAlloc(allocator, ' ', max_read_length) orelse unreachable;
     errdefer allocator.free(method);
 
@@ -162,7 +168,7 @@ fn readRequestLine(reader: *std.net.Stream.Reader) ![3][]u8 {
 }
 
 // Note: `readUntil` reads up to the delimiter. The delimiter is consumed but not returned
-fn readHeader(reader: *std.net.Stream.Reader) ![]u8 {
+fn readHeader(allocator: std.mem.Allocator, reader: *std.net.Stream.Reader) ![]u8 {
     const up_to_delimiter = try reader.readUntilDelimiterOrEofAlloc(allocator, '\r', max_read_length) orelse return error.Empty;
     errdefer allocator.free(up_to_delimiter);
 
@@ -174,10 +180,10 @@ fn readHeader(reader: *std.net.Stream.Reader) ![]u8 {
     return up_to_delimiter;
 }
 
-fn readRequest(reader: *std.net.Stream.Reader) !*Request {
-    const request_line = try readRequestLine(reader);
+fn readRequest(allocator: std.mem.Allocator, reader: *std.net.Stream.Reader) !*Request {
+    const request_line = try readRequestLine(allocator, reader);
 
-    var request = try Request.init();
+    var request = try Request.init(allocator);
     errdefer request.deinit();
 
     request.method = request_line[0];
@@ -190,7 +196,7 @@ fn readRequest(reader: *std.net.Stream.Reader) !*Request {
     }
 
     while (true) {
-        const header_line = try readHeader(reader);
+        const header_line = try readHeader(allocator, reader);
 
         if (header_line.len == 0) {
             break;
@@ -202,21 +208,22 @@ fn readRequest(reader: *std.net.Stream.Reader) !*Request {
     return request;
 }
 
-fn echo(request: *Request, stream: *std.net.Stream) !void {
+fn echo(allocator: std.mem.Allocator, request: *Request, stream: std.net.Stream) !void {
     if (request.segments.items.len != 2) {
         _ = try stream.write("HTTP/1.1 422 Unprocessable Content\r\n\r\n");
         return;
     }
 
-    const response = try Response.text(request.segments.items[1]);
+    const response = try Response.text(allocator, request.segments.items[1]);
     const bytes = try response.pack();
+    defer allocator.free(bytes);
 
     std.debug.print("{s}", .{bytes});
 
     _ = try stream.write(bytes);
 }
 
-fn userAgent(request: *Request, stream: *std.net.Stream) !void {
+fn userAgent(allocator: std.mem.Allocator, request: *Request, stream: std.net.Stream) !void {
     if (request.segments.items.len != 1) {
         _ = try stream.write("HTTP/1.1 422 Unprocessable Content\r\n\r\n");
         return;
@@ -236,10 +243,36 @@ fn userAgent(request: *Request, stream: *std.net.Stream) !void {
         }
     };
 
-    const response = try Response.text(user_agent);
+    const response = try Response.text(allocator, user_agent);
     const bytes = try response.pack();
+    defer allocator.free(bytes);
 
     _ = try stream.write(bytes);
+}
+
+fn do(allocator: std.mem.Allocator, conn: std.net.Server.Connection) !void {
+    var reader = conn.stream.reader();
+
+    const request = try readRequest(allocator, &reader);
+    defer request.deinit();
+
+    if (std.mem.eql(u8, request.target, "/")) {
+        _ = try conn.stream.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else if (std.mem.startsWith(u8, request.target, "/echo/")) {
+        return echo(allocator, request, conn.stream);
+    } else if (std.mem.startsWith(u8, request.target, "/user-agent")) {
+        return userAgent(allocator, request, conn.stream);
+    } else {
+        _ = try conn.stream.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+}
+
+fn handleConnection(allocator: std.mem.Allocator, conn: std.net.Server.Connection) void {
+    defer conn.stream.close();
+
+    do(allocator, conn) catch {
+        _ = conn.stream.write("HTTP/1.1 500 Server Error\r\n\r\n") catch {};
+    };
 }
 
 pub fn main() !void {
@@ -249,20 +282,17 @@ pub fn main() !void {
     });
     defer listener.deinit();
 
-    while (true) {
-        var conn = try listener.accept();
-        var reader = conn.stream.reader();
-        const request = try readRequest(&reader);
-        defer request.deinit();
+    var allocator = std.heap.ThreadSafeAllocator{ .child_allocator = std.heap.page_allocator };
+    var pool: std.Thread.Pool = undefined;
 
-        if (std.mem.eql(u8, request.target, "/")) {
-            _ = try conn.stream.write("HTTP/1.1 200 OK\r\n\r\n");
-        } else if (std.mem.startsWith(u8, request.target, "/echo/")) {
-            return echo(request, &conn.stream);
-        } else if (std.mem.startsWith(u8, request.target, "/user-agent")) {
-            return userAgent(request, &conn.stream);
-        } else {
-            _ = try conn.stream.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        }
+    try pool.init(std.Thread.Pool.Options{
+        .allocator = allocator.child_allocator,
+        .n_jobs = 10,
+    });
+    defer pool.deinit();
+
+    while (true) {
+        const conn = try listener.accept();
+        try pool.spawn(handleConnection, .{ allocator.allocator(), conn });
     }
 }
